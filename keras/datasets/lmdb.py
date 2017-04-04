2042ca97ef8ce6089edaab6610e4b6cb7f51c05e
Rui Wang <rwa56@lib7000-rcmac03.lib.sfu.ca>
2017-04-03 17:16:57 -0700

    two functions

---
+++ keras/datasets/lmdb.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+import lmdb
+from PIL import Image, ImageOps
+import random
+import sys
+
+# Load the dataset to database, build the leveldb
+# from the dataset:
+# input: train.txt Path to the images
+#
+def load_data():
+
+# Write the deep convolutional features of images
+# to database
+
+def write_feature():
