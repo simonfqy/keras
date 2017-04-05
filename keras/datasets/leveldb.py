e33b498be9eb55a95e73e1ddc6f853ee031fb661
Rui Wang <rwa56@lib7000-rcmac03.lib.sfu.ca>
2017-04-04 23:11:34 -0700

    new build

--- keras/datasets/leveldb.py
+++ keras/datasets/leveldb.py
@@ -1,13 +1,21 @@
 import numpy as np
 import os
-import leveldb
+import plyvel
 from PIL import Image, ImageOps
 import random
 import sys
 
+#Build the database
+def build_db(train_file, label_file, db_path,img_size=(224,224,3)):
+    db = plyvel.DB(db_path, create_if_missing=True,, error_if_exists=True, write_buffer_size=268435456)
+    wb = db.write_batch()
 
-# Load the dataset to database:
-def load_data():
+
+    img = Image.open(root+imgfile, 'r')
+
+
+# Load the images from database:
+def load_db():
     train_file = open()
 
 # Write the features to database
